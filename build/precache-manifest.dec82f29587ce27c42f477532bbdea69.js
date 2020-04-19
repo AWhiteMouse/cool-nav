14e1fb69ec969d57e4f21d4ece17f1652a906084
AWhiteMouse <1586671591@qq.com>
2020-04-19 22:44:44 +0800

    feat: 释放build文件夹

---
+++ build/precache-manifest.dec82f29587ce27c42f477532bbdea69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba52a0a5091ee6a21ccb1aa8cce9dba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9300cd6d3a8ebdf97d",
+    "url": "/static/css/2.e9251c5e.chunk.css"
+  },
+  {
+    "revision": "93b605645cac598c8b8e",
+    "url": "/static/css/main.87dd3f59.chunk.css"
+  },
+  {
+    "revision": "ec9300cd6d3a8ebdf97d",
+    "url": "/static/js/2.1113cee3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1113cee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b605645cac598c8b8e",
+    "url": "/static/js/main.b93855b0.chunk.js"
+  },
+  {
+    "revision": "50ad491065f994c8c1f9",
+    "url": "/static/js/runtime-main.f9735e94.js"
+  }
+]);
